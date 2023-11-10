8afe29fc6f7e8f982cb2dbbe75194acdb5c698ad
Dave86dev <davidochando1986@gmail.com>
2023-11-10 10:45:19 +0100

    Comprobación dinámica de errores en formulario

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,87 @@
+import React, { useState, useEffect } from "react";
+import "./Register.css";
 
+import { CustomInput } from "../../common/CustomInput/CustomInput";
+import { validator } from "../../services/useful";
 
-import React from 'react';
-import './Register.css'
 export const Register = () => {
-     return (
-         <div className="registerDesign"></div>
-     )
-}
+
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    phone: ''
+  })
+
+  const [userError, setUserError] = useState({
+    nameError: '',
+    emailError: '',
+    passwordError: '',
+    phoneError: ''
+  })
+
+
+  const functionHandler = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const errorCheck = (e) => {
+
+    let error = "";
+
+    error = validator(e.target.name, e.target.value);
+
+    setUserError((prevState) => ({
+        ...prevState,
+        [e.target.name + 'Error']: error,
+    }));
+  }
+
+  return (
+    <div className="registerDesign">
+      <CustomInput
+        design={`inputDesign ${userError.emailError !== "" ? 'inputDesignError' : ''}`}
+        type={"email"}
+        name={"email"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        functionBlur={errorCheck}
+      />
+      <div className='errorMsg'>{userError.emailError}</div>
+      <CustomInput
+        design={`inputDesign ${userError.passwordError !== "" ? 'inputDesignError' : ''}`}
+        type={"password"}
+        name={"password"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        functionBlur={errorCheck}
+      />
+      <div className='errorMsg'>{userError.passwordError}</div>
+      <CustomInput
+        design={`inputDesign ${userError.nameError !== "" ? 'inputDesignError' : ''}`}
+        type={"text"}
+        name={"name"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        functionBlur={errorCheck}
+      />
+      <div className='errorMsg'>{userError.nameError}</div>
+      <CustomInput
+        design={`inputDesign ${userError.phoneError !== "" ? 'inputDesignError' : ''}`}
+        type={"text"}
+        name={"phone"}
+        placeholder={""}
+        // value={}
+        functionProp={functionHandler}
+        functionBlur={errorCheck}
+      />
+      <div className='errorMsg'>{userError.phoneError}</div>
+    </div>
+  );
+};
